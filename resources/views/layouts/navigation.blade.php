93ac0cbd83f107238d580e743b579884efb5b49c
yuu89hf <yuu89gsseed@gmail.com>
2026-07-15 03:54:35 +0800

    Update logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <!-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> -->
+                        <img src="{{ asset('images/logo.png') }}" alt="Logo CharterB" class="h-8 w-auto">
                     </a>
                 </div>
 
